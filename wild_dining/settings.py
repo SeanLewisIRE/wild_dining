b53054993fb80c80d1b44b95a0b9061dea52e960
SeanLewisIRE <slewis100@gmail.com>
2020-06-18 10:44:49 +0100

    setup static css and media folders

--- wild_dining/settings.py
+++ wild_dining/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
