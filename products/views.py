6c3807e44a046c8e84db2f0fce8227a0e49ad8f3
SeanLewisIRE <slewis100@gmail.com>
2020-06-29 15:05:27 +0100

    addd filtering to navbar and headers. Added search ability

--- products/views.py
+++ products/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
     """ View to render all products including sorting and searching """
     products = Product.objects.all()
+    query = None 
+    categories = None
+    """if statement is to allow searching of products. Search form action links to product view"""
+    
+    if request.GET: 
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET: 
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria entered", redirect(reverse('products')))
+            queries = Q(name__icontains = query) | Q(description__contains = query)
+            products = products.filter(queries)
 
     context = {
-        'products':products,
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)  # context required to allow sending things back to template
