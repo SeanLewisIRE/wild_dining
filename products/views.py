b94653ad77f36c5bf6edf2877bebeb09b9cf72c6
SeanLewisIRE <slewis100@gmail.com>
2020-06-29 17:05:44 +0100

    added soring functionality

--- products/views.py
+++ products/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
     """ View to render all products including sorting and searching """
+    
     products = Product.objects.all()
     query = None 
     categories = None
-    """if statement is to allow searching of products. Search form action links to product view"""
+    sort = None
+    direction = None
     
-    if request.GET: 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -24,10 +41,13 @@ def all_products(request):
             queries = Q(name__icontains = query) | Q(description__contains = query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)  # context required to allow sending things back to template
