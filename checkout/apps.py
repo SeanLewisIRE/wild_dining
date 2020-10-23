a1899ac71bd06d5c4c8a64e51affc3511358f5f2
SeanLewisIRE <slewis100@gmail.com>
2020-10-23 10:27:02 +0100

    Added checkout_success

--- checkout/apps.py
+++ checkout/apps.py
@@ -6,3 +6,6 @@ class CheckoutConfig(AppConfig):
 
     def ready(self):
         import checkout.signals
+
+# Custom ready method, this must be referenced in the __init__ file of this module
+# if not referenced, the signals file that ready() references would not work
