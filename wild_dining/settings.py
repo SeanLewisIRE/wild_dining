de7c5324c0f77a7b93dcc5d7de088de911c4dd1a
SeanLewisIRE <slewis100@gmail.com>
2020-10-26 15:12:55 +0000

    added Hekoku DB

--- wild_dining/settings.py
+++ wild_dining/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from decouple import config
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -96,11 +97,15 @@ WSGI_APPLICATION = 'wild_dining.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(config('HEROKU_DB_URL'))
 }
 
 
